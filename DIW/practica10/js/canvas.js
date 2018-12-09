838bcc87ee13fe8957fa915e95436b098e8edbad
José Rafael Álvarez Espino <joserrafael.ja@gmail.com>
2018-12-09 21:34:36 +0100

    Subida práctica10 DIW

---
+++ DIW/practica10/js/canvas.js
@@ -0,0 +1,29 @@
+
+{
+    window.addEventListener("load", init);
+    let contexto;
+    let image = new Image();
+    image.src = "img/perfil.jpg"
+
+    function init() {
+        contexto = cargarContextoCanvas('canvasPerfil');
+        if (contexto) {
+            recortarImagen();
+        }
+    }
+
+    function recortarImagen(){
+        contexto.drawImage(image,0,0,180,200);
+
+    }
+
+    function cargarContextoCanvas(id) {
+        let elemento = document.getElementById(id);
+        if (elemento && elemento.getContext) {
+            let contexto = elemento.getContext('2d');
+            if (contexto)
+                return contexto;
+        }
+        return false;
+    }
+}
