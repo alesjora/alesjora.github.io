6194de71158027a8a5529072d493f997ac0788ff
José Rafael Álvarez Espino <joserrafael.ja@gmail.com>
2019-01-03 19:47:39 +0100

    Prueba practica 12

---
+++ practica12/js/main.js
@@ -0,0 +1,36 @@
+{
+    window.addEventListener("load", init);
+    let elementoMenu;
+    let myTopnav;
+    let myHeader;
+
+    function init() {
+        myTopnav = document.getElementById("myTopnav");
+        myHeader = document.getElementById("myHeader");
+        flechaAbajo = document.getElementById("flechaAbajo");
+        elementoMenu = document.getElementById("abrirMenu");
+        elementoMenu.addEventListener("click", funcionResponsive);
+        
+    }
+
+    function funcionResponsive() {
+        if (myTopnav.className === "topnav") {
+            myTopnav.className += "responsive";
+            // myHeader.style.backgroundColor = "gray";
+        } else {
+            myTopnav.className = "topnav";
+            myHeader.style.backgroundColor = "transparent";
+        }
+
+    }
+
+    $(document).ready(function() {
+        $("#flechaAbajo").click(function () {
+            $('html,body').animate({
+                scrollTop: $("main").offset().top
+            }, 500);
+        });
+    
+    });
+
+}
