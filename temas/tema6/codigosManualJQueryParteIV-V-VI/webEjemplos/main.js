913afb1d7c8ea15852c7171e7f7e9a145d000487
José Rafael Álvarez Espino <joserrafael.ja@gmail.com>
2019-01-19 12:44:45 +0100

    Corregido deshabilitar checkboxs

--- temas/tema6/codigosManualJQueryParteIV-V-VI/webEjemplos/main.js
+++ temas/tema6/codigosManualJQueryParteIV-V-VI/webEjemplos/main.js
@@ -5,26 +5,26 @@
     $(function () {
         $informacion1 = $("#informacion1");
         let coordenadas = $("#coordenadas");
-        $(document).mousemove(function(e){
-            coordenadas.html("X: " + e.pageX + " - Y: " + e.pageY + " timeStamp: "+e.timeStamp)
+        $(document).mousemove(function (e) {
+            coordenadas.html("X: " + e.pageX + " - Y: " + e.pageY + " timeStamp: " + e.timeStamp)
         });
         $checkboxs = $(":checkbox");
-        $checkboxs.click(deshabilitarCheckBox);
-        $( "#ejemploOn" ).on( "mouseenter click", function() {
+        $checkboxs.change(deshabilitarCheckBox);
+        $("#ejemploOn").on("mouseenter click", function () {
             console.log("A este mensaje me refería.");
         });
         $(":radio").click(mostrarInformacionRadioButton);
     });
 
-    function deshabilitarCheckBox(e){
-        $checkboxs.each(function (){
-            if (e.currentTarget.id != $(this).prop("id"))
-                $(this).prop("checked", false);
-        });
+    function deshabilitarCheckBox(e) {
+        if ($(this).prop("checked"))
+            $checkboxs.not(":checked").prop("disabled", true);
+        else
+            $checkboxs.not(":checked").prop("disabled", false);
     }
 
-    function mostrarInformacionRadioButton(){
+    function mostrarInformacionRadioButton() {
         $radioSeleccionado = $(":checked");
-        $informacion1.html("Radiobutton seleccionado mediante :checked -> "+$radioSeleccionado.prop("id"));
+        $informacion1.html("Radiobutton seleccionado mediante :checked -> " + $radioSeleccionado.prop("id"));
     }
 }
