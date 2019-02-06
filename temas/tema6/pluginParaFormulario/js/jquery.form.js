4e3a5d1bd650782dcaf36f810b10204d784b9e89
José Rafael Álvarez Espino <joserrafael.ja@gmail.com>
2019-02-06 16:57:40 +0100

    Añadido ajax y modificaciones varias

--- temas/tema6/pluginParaFormulario/js/jquery.form.js
+++ temas/tema6/pluginParaFormulario/js/jquery.form.js
@@ -3,8 +3,14 @@
   let patrones = {
     nombre: [/([a-zA-Z]{3,}\s?){1,3}/, "Mínimo un nombre de tres caracteres."],
     apellidos: [/([a-zA-Z]{3,}\s?){1,3}/, "Mínimo un apellido de tres caracteres."],
-    email: [/^[\w]+@{1}[\w]+\.[a-z]{2,}/i, "Formato correo no válido."],
+    email: [/^[\w]{2,}\.?[\w]*@{1}[\w]+[\.\w]/i, "Formato correo no válido."],
     mensaje: [/(\w){10,}/, "Mínimo 10 caracteres."]
+  }
+
+  let tester = {
+    validar (texto, patron) {
+      return patrones[patron][0].test(texto);
+    }
   }
 
   $.fn.validar = function (estilos2) {
@@ -16,7 +22,7 @@
 
     $.extend(estilos, estilos2);
 
-    let $inputs = $(":input").not(":submit",$(this));
+    let $inputs = $(":input[type='text'],textarea",$(this));
     let inputsErroneos = [];
     let $inputActual;
     let patron;
@@ -24,7 +30,7 @@
     $inputs.blur(function () {
       $inputActual = $(this);
       patron = $inputActual.attr("tipo");
-      if (!patrones[patron][0].test($inputActual.val())) {
+      if (!tester.validar($inputActual.val(),patron)) {
         $(this).css(estilos);
         inputsErroneos.push($inputActual);
       } else {
@@ -45,13 +51,28 @@
       });
 
     });
+
     $(this).submit(function (event) {
       event.preventDefault();
       inputsErroneos = [];
       $inputs.blur();
-      if(inputsErroneos.size !== 0)
+      if (inputsErroneos.length !== 0)
         inputsErroneos[0].focus();
+      else
+        enviarFormulario();
     });
+
+    function enviarFormulario() {
+      $.ajax({
+        url: "https://alesjora.github.io/temas/tema6/pluginParaFormulario/fichero.txt",
+        success: function (response) {
+          $("#mensaje").val(response);
+        },
+        fail: function () {  
+          console.log("error");
+        }
+      });
+    }
 
     return this;
   }
